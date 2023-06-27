a8ae38da8d6e885aac4f9593fae16dff6f7a7fd5
RICMED <ricmed241323@gmail.com>
2023-06-27 11:32:54 +0530

    HomePage Updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,45 +7,53 @@ import { DatabaseService } from '../database.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-url:string[];
-img="./assets/BURGER.png";
-i=0;
-color="";
+
+// -------------------------------- Routing Modules--------------------
+
 Relevance:string="";
 rating:string="rating";
 Delivery_Time:string="Delivery_Time";
 low_to_high:string="low_to_high";
 high_to_low:string="high_to_low";
 filter:string="filter";
-hotelarray:any=[];
+
+// ---------------------------------------------------------------------
+
+hotelArray:any=[];
 hotelCount=0;
+slideImageUrl:string[];
+slideImage="./assets/BURGER.png";
+countingVariable=0;
+
+
 constructor(private hotel:DatabaseService){
-  this.url=["./assets/BURGER.png","./assets/FLAT_50.png"
+  this.slideImageUrl=["./assets/BURGER.png","./assets/FLAT_50.png"
 ,"./assets/pizza.png","./assets/FOOD COURT.png"];
-this.hotelList();
+this.HotelList();
 }
 
-hotelList(){
+HotelList(){
   this.hotel.read_hotels().subscribe(x=>{
-    this.hotelarray=x;
-    this.hotelCount=this.hotelarray.length;
+    this.hotelArray=x;
+    this.hotelCount=this.hotelArray.length;
   })
 }
 
-changeImage(ind:number){
-  this.img=this.url[ind];
-  this.i=ind;
+SlideImages(ind:number){
+  this.slideImage=this.slideImageUrl[ind];
+  this.countingVariable=ind;
 }
+
+
 ngOnInit(){
   setInterval(()=>{
-    if(this.i==-1){
-      this.i;
+    if(this.countingVariable==-1){
+      this.countingVariable;
     }
-    else if(this.i==4){
-      this.i=0;
+    else if(this.countingVariable==4){
+      this.countingVariable=0;
     }
-    this.img=this.url[this.i++];
-    // this.color="orange";
+    this.slideImage=this.slideImageUrl[this.countingVariable++];
     },3000)
 }
 
